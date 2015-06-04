b7757c5bf835d33a8826a36b46bb9ca3c38e50f2
Roman Koshelev <romakosh@gmail.com>
2015-06-04 22:44:35 +0300

    binary search start

--- Sources/School.Lybrary/Searchers/BinarySearcher.cs
+++ Sources/School.Lybrary/Searchers/BinarySearcher.cs
@@ -12,11 +12,17 @@ namespace School.Lybrary.Searchers
     {
         protected override int DoSearch( int[] array, int value )
         {
-            return BinarySearch( array, value );
+            return BinarySearchRecursive( array, value );
         }
 
-        private int BinarySearch( int[] array, int value )
+        private int BinarySearchRecursive( int[] array, int value )
         {
+            // Todo:> BinarySearch http://en.wikipedia.org/wiki/Binary_search_algorithm
+            return 0;
+        }
+        private int BinarySearchIterative( int[] array, int value )
+        {
+            // Todo:> BinarySearch http://en.wikipedia.org/wiki/Binary_search_algorithm
             return 0;
         }
     }
