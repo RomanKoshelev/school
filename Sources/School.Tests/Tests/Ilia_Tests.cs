e943bdb7032192fe5d3bec552a9b6d280de2225a
Roman Koshelev <romakosh@gmail.com>
2015-10-18 19:53:24 +0300

    [Just refresh repository]

--- Sources/School.Tests/Tests/Ilia_Tests.cs
+++ Sources/School.Tests/Tests/Ilia_Tests.cs
@@ -11,7 +11,6 @@ namespace School.Nunit.Tests
     public class Ilia
     {
         #region Console
-
         private static void Print( object arg )
         {
             Console.WriteLine( arg.ToString() );
@@ -26,7 +25,6 @@ namespace School.Nunit.Tests
         {
             return 6;
         }
-
         #endregion
 
 
@@ -68,7 +66,7 @@ namespace School.Nunit.Tests
             int i;
             n = 2500;
 
-            for( i = 25; i < n; i = i + 1000 ) {
+            for( i = 25; i < n; i = i + 10000 ) {
                 Print( i );
             }
 
@@ -113,37 +111,167 @@ namespace School.Nunit.Tests
             if( n < 10 ) {
                 Print( "{0}<10", n );
                 Print( "(Yes)" );
-            } else {
+            }
+            else {
                 Print( "{0}>=10", n );
                 Print( "(No)" );
-                if( n > 301 ) {
+                if( n > 301 )
                     Print( "Wow! " );
-                }
             }
-            if( n < 1000 ) {
+            if( n < 1000 )
                 Print( "why?" );
-            }
         }
 
+        [Test]
         [TestCase( "man", 5, "small" )]
         [TestCase( "man", 116, "big" )]
         [TestCase( "cat", 11, "big" )]
         [TestCase( "cat", 3, "small" )]
+        [TestCase( "whale", 3000, "small" )]
+        [TestCase( "whale", 1000, "small" )]
+        [TestCase( "whale", 3001, "big" )]
+        [TestCase( "whale", 5000, "big" )]
         [TestCase( "ant", 100, "unknown" )]
         public void Big_or_small( string kind, int weight, string answer )
         {
             var result = "unknown";
 
-            if( kind == "man" ) {
+            if( kind == "man" )
                 result = weight > 70 ? "big" : "small";
+
+            if( kind == "cat" )
+                result = weight > 10 ? "big" : "small";
+
+            if( kind == "whale" )
+                result = weight > 3000 ? "big" : "small";
+
+            Print( "{0} {1} kg = {2}", kind, weight, result );
+            Assert.That( result, Is.EqualTo( answer ) );
+        }
+
+        [Test]
+        [TestCase( "man", 1, "small" )]
+        [TestCase( "man", 5, "small" )]
+        [TestCase( "man", 50, "small" )]
+        [TestCase( "man", 59, "small" )]
+        [TestCase( "man", 60, "normal" )]
+        [TestCase( "man", 110, "normal" )]
+        [TestCase( "man", 111, "big" )]
+        [TestCase( "man", 200, "big" )]
+        [TestCase( "man", 299, "big" )]
+        [TestCase( "man", 300, "fat" )]
+        [TestCase( "man", 500, "fat" )]
+        [TestCase( "man", 10000, "fat" )]
+        [TestCase( "mouse", 1, "big" )]
+        public void Big_or_normal_or_small( string kind, int weight, string answer )
+        {
+            var result = "unknown";
+
+            if( kind == "man" ) {
+                Print( "- Man" );
+
+                if( weight > 299 && weight < 10000000 ) {
+                    result = "fat";
+                    Print( "-- Fat" );
+                }
+
+                if( weight > 110 && weight < 300 ) {
+                    result = "big";
+                    Print( "-- Big" );
+                }
+
+                if( weight > 59 && weight < 111 ) {
+                    result = "normal";
+                    Print( "-- Normal" );
+                }
+
+                if( weight > 0 && weight < 60 ) {
+                    result = "small";
+                    Print( "-- Small" );
+                }
             }
 
-            if( kind == "cat" ) {
-                result = weight > 10 ? "big" : "small";
+            if( kind == "mouse" ) {
+                Print( "- Mouse" );
+                if( weight > 0 && weight < 2 ) {
+                    result = "big";
+                    Print( "-- Big" );
+                }
             }
 
             Print( "{0} {1} kg = {2}", kind, weight, result );
             Assert.That( result, Is.EqualTo( answer ) );
+        }
+
+        [TestCase( new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1 }, 10 )]
+        [TestCase( new[] { 6, 19, 1, 4 }, 30 )]
+        [TestCase( new[] { 10, 7, 6 }, 23 )]
+        [TestCase( new[] { 1, 2, 1 }, 4 )]
+        [TestCase( new[] { 0, 1, 2 }, 3 )]
+        [TestCase( new[] { 100, -100 }, 0 )]
+        [TestCase( new[] { 42 }, 42 )]
+        [TestCase( new int[] { }, 0 )]
+        public void Money_sum( int[] money, int sum )
+        {
+            var result = 0;
+            var i = 0;
+
+            while( i < money.Length ) {
+                result = result + money[ i ];
+                Print( money[ i ] );
+                i = i + 1;
+            }
+
+            Print( "=================" );
+            Print( result );
+
+            Assert.That( result, Is.EqualTo( sum ) );
+        }
+
+        [TestCase( new[] { 1, 2, 6 }, false )]
+        [TestCase( new[] { 4, 62, 7, 42 }, true )]
+        [TestCase( new[] { 4, 62, 7, 42, 157 }, true )]
+        public void Is_sense_of_life( int[] digits, bool answer )
+        {
+            var result = false;
+            var i = 0;
+
+            while( i < digits.Length ) {
+                if( digits[ i ] == 42 )
+                    result = true;
+
+                Print( digits[ i ] );
+                i = i + 1;
+            }
+            Print( result );
+
+            Assert.That( result, Is.EqualTo( answer ) );
+        }
+
+        [Test]
+        [TestCase( new[] { 1, 3, 2 }, 1 )]
+        [TestCase( new[] { 5, 8, 25, 300, 287, 0 }, 3 )]
+        [TestCase( new[] { 1, 2, 29 ,5,188, 1488, 42 }, 5 )]
+        public void Best_strategy( int[] arr, int pos )
+        {
+            var r = -1;
+            var i = 0;
+
+            while( i < arr.Length ) {
+                Print( arr[ i ] );
+                if( r == -1 )
+                    r = 0;
+                if( arr[ i ] >= arr[ r ] ) {
+                    Print( "Gotcha!" );
+                    r = i;
+                }
+
+                i = i + 1;
+            }
+
+            Print( "----" );
+            Print( "{0}->{1}", r, arr[ r ] );
+            Assert.That( r, Is.EqualTo( pos ) );
         }
     }
 }
